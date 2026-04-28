88031e1447d133ad0d55a2bbe9f207de3449fe9f
Kekovich23 <mr.kekovich@ya.ru>
2026-04-28 21:30:33 +0700

    #9 add webAPI project and migrations

--- DriveLog.Domain/Entities/RaceLap.cs
+++ DriveLog.Domain/Entities/RaceLap.cs
@@ -2,14 +2,27 @@
 
 namespace DriveLog.Domain.Entities;
 
-public class RaceLap(Race race, Driver driver, Car car, int lapNumber, TimeSpan lapTime) : BaseEntity<Guid> {
-    public Guid RaceId { get; private set; } = race.Id;
-    public int LapNumber { get; private set; } = lapNumber;
-    public TimeSpan LapTime { get; private set; } = lapTime;
-    public Guid DriverId { get; private set; } = driver.Id;
-    public Guid CarId { get; private set; } = car.Id;
+public class RaceLap : BaseEntity<Guid> {
+    public RaceLap(Race race, Driver driver, Car car, int lapNumber, TimeSpan lapTime) {
+        RaceId = race.Id;
+        LapNumber = lapNumber;
+        LapTime = lapTime;
+        DriverId = driver.Id;
+        CarId = car.Id;
+        Race = race;
+        Driver = driver;
+        Car = car;
+    }
 
-    public Race Race { get; private set; } = race;
-    public Driver Driver { get; private set; } = driver;
-    public Car Car { get; private set; } = car;
+    protected RaceLap() { }
+
+    public Guid RaceId { get; private set; }
+    public int LapNumber { get; private set; }
+    public TimeSpan LapTime { get; private set; }
+    public Guid DriverId { get; private set; }
+    public Guid CarId { get; private set; }
+
+    public Race Race { get; private set; } = null!;
+    public Driver Driver { get; private set; } = null!;
+    public Car Car { get; private set; } = null!;
 }
