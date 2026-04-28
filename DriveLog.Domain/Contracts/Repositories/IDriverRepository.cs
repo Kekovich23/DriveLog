3462416e76d85f64fc1edbb4519f530e22e8b2f7
Kekovich23 <mr.kekovich@ya.ru>
2026-04-28 13:33:07 +0700

    #6 add repositories contracts

---
+++ DriveLog.Domain/Contracts/Repositories/IDriverRepository.cs
@@ -0,0 +1,6 @@
+﻿using DriveLog.Domain.Contracts.Repositories.Base;
+using DriveLog.Domain.Entities;
+
+namespace DriveLog.Domain.Contracts.Repositories;
+
+public interface IDriverRepository : IRepository<Driver, Guid>;
