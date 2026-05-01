f73570cab2ad320df2dd7dd16057e4fe900160ef
Kekovich23 <mr.kekovich@ya.ru>
2026-05-01 19:21:29 +0700

    #11 add ApplicationServices

---
+++ DriveLog.Application.Services/Contracts/IRaceLapApplicationService.cs
@@ -0,0 +1,6 @@
+﻿using DriveLog.Application.Models.RaceLap;
+using DriveLog.Application.Services.Contracts.Base;
+
+namespace DriveLog.Application.Services.Contracts;
+
+public interface IRaceLapApplicationService : IApplicationService<RaceLapModel, RaceLapCreateModel, Guid>;
