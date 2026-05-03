21777fe685e30f50112dfc0949796a2cdf1a47e4
Kekovich23 <mr.kekovich@ya.ru>
2026-05-04 02:05:12 +0700

    #16 config web

--- DriveLog.WebAPI/Mapping/ApplicationProfile.cs
+++ DriveLog.WebAPI/Mapping/ApplicationProfile.cs
@@ -5,8 +5,10 @@ using DriveLog.Application.Models.Race;
 using DriveLog.Application.Models.RaceLap;
 using DriveLog.Application.Models.Track;
 using DriveLog.Domain.Entities;
+using DriveLog.WebAPI.Models.Requests;
+using DriveLog.WebAPI.Models.Responses;
 
-namespace DriveLog.Application.Services.Mapping;
+namespace DriveLog.WebAPI.Mapping;
 
 public class ApplicationProfile : Profile {
     public ApplicationProfile() {
@@ -22,5 +24,23 @@ public class ApplicationProfile : Profile {
         CreateMap<RaceLap, RaceLapModel>();
         CreateMap<Track, TrackModel>()
             .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name.Value));
+
+        CreateMap<CarRequestModel, CarModel>();
+        CreateMap<DriverRequestModel, DriverModel>();
+        CreateMap<RaceRequestModel, RaceModel>();
+        CreateMap<RaceLapRequestModel, RaceLapModel>();
+        CreateMap<TrackRequestModel, TrackModel>();
+
+        CreateMap<CarRequestModel, CarCreateModel>();
+        CreateMap<DriverRequestModel, DriverCreateModel>();
+        CreateMap<RaceRequestModel, RaceCreateModel>();
+        CreateMap<RaceLapRequestModel, RaceLapCreateModel>();
+        CreateMap<TrackRequestModel, TrackCreateModel>();
+
+        CreateMap<CarModel, CarResponseModel>();
+        CreateMap<DriverModel, DriverResponseModel>();
+        CreateMap<RaceModel, RaceResponseModel>();
+        CreateMap<RaceLapModel, RaceLapResponseModel>();
+        CreateMap<TrackModel, TrackResponseModel>();
     }
 }
--- DriveLog.WebAPI/Program.cs
+++ DriveLog.WebAPI/Program.cs
@@ -1,26 +1,28 @@
-﻿using DriveLog.Infrastructure;
-using Microsoft.EntityFrameworkCore;
+﻿using DriveLog.WebAPI.Extensions;
 
 DotNetEnv.Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<DriveLogDbContext>(opt => {
-    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
-
-    if (builder.Environment.IsDevelopment()) {
-        opt.EnableSensitiveDataLogging();
-        opt.EnableDetailedErrors();
-    }
-});
+builder.AddApplicationServices();
 
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options => {
+    var modelsXmlPath = Path.Combine(AppContext.BaseDirectory, "DriveLog.WebAPI.Models.xml");
+    if (File.Exists(modelsXmlPath)) {
+        options.IncludeXmlComments(modelsXmlPath);
+    }
+});
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment()) {
-    app.MapOpenApi();
+    app.UseSwagger(options => options.RouteTemplate = "swagger/{documentName}/swagger.json");
+    app.UseSwaggerUI(options => {
+        options.RoutePrefix = string.Empty;
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "DriveLog API v1");
+    });
 }
 
 app.UseHttpsRedirection();
