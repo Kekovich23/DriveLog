2337454d721c57f8dadecfbfaead254463628c29
Kekovich23 <mr.kekovich@ya.ru>
2026-06-03 00:06:10 +0700

    fix race queries

--- Application/DriveLog.Application.Services/RaceQueryService.cs
+++ Application/DriveLog.Application.Services/RaceQueryService.cs
@@ -19,54 +19,73 @@ public class RaceQueryService(DriveLogDbContext context) : IRaceQueryService {
         => new(await context.Races.AsNoTracking()
                                   .Skip(skip)
                                   .Take(take)
-                               .Select(r => new RaceModel(r.Id, r.TrackId, r.Date.Value))
+                                  .Select(r => new RaceModel(r.Id, r.TrackId, r.Date.Value))
                                   .ToListAsync(cancellationToken),
                await context.Races.CountAsync(cancellationToken));
 
     public async Task<BestTimeDto?> GetBestLapTimeAsync(Guid raceId, CancellationToken cancellationToken = default) {
-        var bestLapData = await context.Races.AsNoTracking()
-                          .Where(r => r.Id == raceId)
-                          .SelectMany(r => r.Entries.SelectMany(e => e.Laps.Select(l => new {
-                              e.DriverId,
-                              e.CarId,
-                              LapNumber = l.Id.Value,
-                              l.Time.Duration
-                          }))).OrderBy(l => l.Duration).FirstOrDefaultAsync(cancellationToken);
+        var raceData = await context.Races.AsNoTracking()
+                                             .Where(r => r.Id == raceId)
+                                             .SelectMany(r => r.Entries.Where(e => e.Laps.Count != 0).Select(e => new {
+                                                 e.DriverId,
+                                                 e.CarId,
+                                                 e.Laps
+                                             }))
+                                             .ToListAsync(cancellationToken);
 
-        if (bestLapData == null) {
+
+
+        if (raceData == null || raceData.Count == 0) {
             return null;
         }
+
+        var bestLapData = raceData.Select(x => new {
+            x.DriverId,
+            x.CarId,
+            BestLap = x.Laps.OrderBy(l => l.Time.Duration).First(),
+        })
+                                  .OrderByDescending(x => x.BestLap.Id.Value)
+                                  .ThenBy(x => x.BestLap.Time.Duration)
+                                  .First();
 
         var driver = await context.Drivers.AsNoTracking().FirstOrDefaultAsync(d => d.Id == bestLapData.DriverId, cancellationToken)
             ?? throw new EntityNotFoundException("Driver not found.");
         var car = await context.Cars.AsNoTracking().FirstOrDefaultAsync(c => c.Id == bestLapData.CarId, cancellationToken)
             ?? throw new EntityNotFoundException("Car not found.");
 
-        return bestLapData == null
-            ? null
-            : new BestTimeDto(
-            $"{driver.Name.FirstName} {driver.Name.LastName}",
+        return new BestTimeDto(
+            driver.Name.ToString(),
             car.Number.Value,
-            bestLapData.LapNumber,
-            bestLapData.Duration
+            bestLapData.BestLap.Id.Value,
+            bestLapData.BestLap.Time.Duration
         );
     }
 
-    public Task<RaceResultsDto?> GetTotalResultsAsync(Guid raceId, CancellationToken cancellationToken = default)
-        => context.Races.AsNoTracking()
-                        .Where(r => r.Id == raceId)
-                        .Select(r => new RaceResultsDto(r.Id,
-                                                        context.Tracks.First(t => t.Id == r.TrackId).Name.Value,
-                                                        r.Date.Value,
-                                                        r.Entries.Select(e => new DriverResultDto(
-                                                            $"{context.Drivers.First(d => d.Id == e.DriverId).Name.FirstName} {context.Drivers.First(d => d.Id == e.DriverId).Name.LastName}",
-                                                            context.Cars.First(c => c.Id == e.CarId).Number.Value,
-                                                            e.Laps.Count,
-                                                            e.Laps.Select(l => l.Time.Duration).Aggregate((t1, t2) => t1 + t2)
-                                                            )
-                                                        )
-                            .OrderByDescending(d => d.TotalLaps)
-                            .ThenBy(d => d.TotalLaps)
-                            .ToList()))
-        .FirstOrDefaultAsync(cancellationToken);
+    public async Task<RaceResultsDto?> GetTotalResultsAsync(Guid raceId, CancellationToken cancellationToken = default) {
+        var raceResults = await context.Races.AsNoTracking()
+                                             .Where(r => r.Id == raceId)
+                                             .Select(r => new {
+                                                 r.Id,
+                                                 TrackName = context.Tracks.First(t => t.Id == r.TrackId).Name,
+                                                 r.Date,
+                                                 Entries = r.Entries.Select(e => new {
+                                                     Driver = context.Drivers.First(d => d.Id == e.DriverId),
+                                                     Car = context.Cars.First(c => c.Id == e.CarId),
+                                                     e.Laps,
+                                                 })
+                                             })
+                                             .FirstOrDefaultAsync(cancellationToken: cancellationToken);
+
+        return raceResults == null
+            ? null
+            : new(raceResults.Id,
+                  raceResults.TrackName.Value,
+                  raceResults.Date.Value,
+                  [.. raceResults.Entries.Select(e => new DriverResultDto(e.Driver.Name.ToString(),
+                                                                          e.Car.Number.Value,
+                                                                          e.Laps.Count,
+                                                                          e.Laps.Select(l => l.Time.Duration).Aggregate((t1, t2) => t1 + t2)))
+                                         .OrderByDescending(x => x.TotalLaps)
+                                         .ThenBy(x => x.TotalTime)]);
+    }
 }
